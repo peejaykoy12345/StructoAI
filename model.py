a9685747b66a57473d8ae759cb45440c4a7fbd96
peejaykoy12345 <rick.well.rick@gmail.com>
2025-07-01 22:26:00 +0800

    Modified model.py to add the training function of it

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader, Dataset
 
@@ -12,7 +13,16 @@ class DocNet(nn.Module):
     def forward(self, x):
         return self.model(x)
     
-    def fit(self, epochs):
-        pass
+    def fit(self, train_loader, epochs):
+        optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
+        criterion = nn.CrossEntropyLoss()
+
+        for epoch in range(epochs):
+            for x, y in train_loader:
+                optimizer.zero_grad()
+                y_pred = self.model(x)
+                loss = criterion(y_pred, y)
+                loss.backward()
+                optimizer.step()
 
     
